3a961283b307ac5f896ed6510848a62fe05ca49e
Monica <monicaestellaw@gmail.com>
2016-08-15 15:16:24 -0700

    manual merge of toString

---
+++ spec/toString_spec.js
@@ -0,0 +1,24 @@
+import { toString } from '../dist/toString.js'
+
+describe('toString', () => {
+	fit('converts an input to a string', () => {
+		expect(toString([1,2,3])).toEqual('1,2,3')
+	})
+
+	it('returns an empty string for a null input', () => {
+		expect(toString(null)).toEqual('')
+	})
+
+	it('returns an empty string for a undefined input', () => {
+		expect(toString(undefined)).toEqual('')
+	})
+
+	it('returns a string for an input number', () => {
+		expect(toString(17302)).toEqual('17302')
+	})
+
+	it('returns only the first number in a series of numbers', () => {
+		expect(toString(1830,2,3)).toEqual('1830')
+	})
+
+})
---
+++ src/toString.js
@@ -0,0 +1,5 @@
+const toString = () => {
+  
+}
+
+export { toString }
