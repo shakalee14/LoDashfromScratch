b912295f9227af7a28f38576bdc854bb866e048b
Moniarchy <monicaestellaw@gmail.com>
2016-08-16 15:23:25 -0700

    Update lte.js

--- src/lte.js
+++ src/lte.js
@@ -1,13 +1,5 @@
-const lte = ( input1, input2 ) => {
-  if( input1 <= input2 ) {
-  return true
-  
-  	} else {
-	return false 
-  }
-}
+const lte = ( input1, input2 ) => input1 <= input2 
 
 
 export { lte }
-
 
