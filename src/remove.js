81ea2522c55c201b417adf256c944b0cb80bbade
Moniarchy <monicaestellaw@gmail.com>
2016-08-18 14:57:58 -0700

    Update remove.js

--- src/remove.js
+++ src/remove.js
@@ -1,7 +1,9 @@
 const remove = (array, fn) => {
-if(array === null || array === undefined){ return undefined}
-  let result = []
-  for(let index = 0; index < array.length; index++){
+if(array === null || array === undefined){ 
+  return undefined
+}
+let result = []
+ for(let index = 0; index < array.length; index++){
     let item = array[index]
     if (fn(item)){
     result.push(item)
