5d13c15b6963602eda7c8fbec9c24376b566342e
John Hopkins <johnthopkins@gmail.com>
2016-08-19 10:44:39 -0700

    made changes per jrobs suggestions

--- src/map.js
+++ src/map.js
@@ -1,21 +1,38 @@
+// const map1 = ( array, operation ) => {
+//   const fn = operation || ( () => {} )
+//   let results =[]
+//   if(typeof operation === 'function'){
+//     for(let index = 0; index < array.length; index++ ){
+//       let item = array[index]
+//       let result = fn(item)
+//       results.push(fn( array[ index ] ))
+//     }
+//   }
+//   else if (typeof operation === 'string'){
+//     for(let index = 0; index < array.length; index++ ){
+//       let item = array[index]
+//       let result = item[operation]
+//       results.push(fn( array[ index ] ))
+//     }
+//   }
+//   return results
+// }
+
+
 const map = ( array, operation ) => {
-  const fn = operation || ( () => {} )
-  let results =[]
-  if(typeof operation === 'function'){
-    for(let index = 0; index < array.length; index++ ){
+  if(operation === null || operation === undefined ){ 
+  return []
+  }else{
+    const fn = operation  || ( a => a )
+    let results =[]
+    for(let index in array ){
       let item = array[index]
-      let result = fn(item)
-      results.push(result)
+      let result = typeof( operation ) === 'function' ? fn( item, index, array ) : item[ operation ]
+      console.log(result)
+      results.push( result )
     }
+    return results
   }
-  else if (typeof operation === 'string'){
-    for(let index = 0; index < array.length; index++ ){
-      let item = array[index]
-      let result = item[operation]
-      results.push(result)
-      console.log(operation)
-    }
-  }
-  return results
 }
+
 export { map }
